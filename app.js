265fc0eefaa43823b071df71e81e3a5060607ca5
Gautam Krishnan <gautam247gk@gmail.com>
2021-09-22 16:41:29 +0530

    test pls

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname));
+var http = require("http").Server(app);
+var WebSocketServer = require("ws").Server;
+var webSocketServer;
+const PORT = 3000;
+var portid = require("./portid");
+const SerialPort = require("serialport");
+const port = new SerialPort(portid.port, {
+  baudRate: 115200,
+  dataBits: 8,
+  parity: "none",
+  stopBits: 1,
+  flowControl: false,
+});
+
+server = http.listen(PORT, () => {
+  console.log(`Server is up and running at ${PORT}`);
+});
+
+webSocketServer = new WebSocketServer({ server: server });
+
+webSocketServer.on("connection", function (socket) {
+  console.log("New client connection");
+  port.write("start", function (err) {
+    console.log("writing start to esp32");
+    if (err) {
+      return console.log("Error on write: ", err.message);
+    }
+  });
+  port.on("data", async function (data) {
+    pressed = true;
+    data = await data.toString("utf-8").trim();
+    console.log(typeof data);
+    console.log("movement from esp32:", data);
+    socket.send(data);
+  });
+});
+
+app.get("/", (req, res) => {
+  //send html file
+  res.sendFile(__dirname + "/index.html");
+});
